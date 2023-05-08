0d33818b60ec97ed93bfd22b7e013fe6ab8e3d89
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-05-08 17:59:30 -0400

    s05-109 Time to practice: adding a new input
    - my solution

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,10 +9,12 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -29,14 +31,26 @@ class _NewExpenseState extends State<NewExpense> {
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text('Amount'),
+            ),
+          ),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
-                  print(_titleController.text);
+                  print('${_titleController.text}, ${_amountController.text}');
                 },
                 child: const Text('Save Expense'),
               ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Cancel'),
+              )
             ],
           ),
         ],
