e25f402ceb2986dbd7f5024b27f32fea74e716c6
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-05-09 19:10:57 -0400

    s05-116 Validating user input & showing an error dialog

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -36,7 +36,21 @@ class _NewExpenseState extends State<NewExpense> {
     if (_titleController.text.trim().isEmpty ||
         amountIsInvalid ||
         _selectedDate == null) {
-      // show error message
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid input'),
+          content: const Text(
+              'Please make sure a valid title, amount date and category was entered.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(ctx),
+              child: const Text('Ok'),
+            ),
+          ],
+        ),
+      );
+      return;
     }
   }
 
